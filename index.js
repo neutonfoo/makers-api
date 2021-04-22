87b25cca96778aca05585d5f01aadaf5aafbe091
Neuton Foo <neuton.foo@gmail.com>
2021-04-21 18:54:10 -0700

    Added insert endpoint

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const people = [];
+
 // GET and POST routes
 app.get("/", (req, res) => {
-  console.log("In / route!");
-  res.send("Hello world");
+  // console.log("In / route!");
+  // res.send("Hello world");
+  res.json(people);
+});
+
+// Insert endpoint
+app.post("/insert", (req, res) => {
+  people.push("Alex");
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
